100f58ced5fc2630b685a23e5c71356e4db4c424
Benjamin Knol <benjamin_knol@knolbeheer.nl>
2025-05-02 01:03:15 +0200

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Benjamin Knol',
+            'email' => 'benjaminknol@shcscoop.nl',
+            'password' => bcrypt('HuizentoernooiZonderWijWonenIsNietHetzelfde'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Youp Evers',
+            'email' => 'youpevers@shcscoop.nl',
+            'password' => bcrypt('EenOrigineelWachtwoordVoorYoup')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Tygo Hilting',
+            'email' => 'tygohilting@shcscoop.nl',
+            'password' => bcrypt('EenOrigineelWachtwoordVoorTygo')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Ingmar Huizing',
+            'email' => 'ingmarhuizing@shcscoop.nl',
+            'password' => bcrypt('EenOrigineelWachtwoordVoorIngmar')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Rithik Putatunda',
+            'email' => 'rithikputatunda@shcscoop.nl',
+            'password' => bcrypt('EenOrigineelWachtwoordVoorRithik')
+        ]);
+    }
+}
---
+++ routes/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::middleware('guest')->group(function () {
+    Volt::route('login', 'auth.login')
+        ->name('login');
+});
+
+Route::post('logout', App\Livewire\Actions\Logout::class)
+    ->name('logout');

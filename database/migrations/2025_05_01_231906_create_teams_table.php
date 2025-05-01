7fd9976c10d3e180c4ae035d36a813f0d0c9a879
Benjamin Knol <benjamin_knol@knolbeheer.nl>
2025-05-02 01:21:39 +0200

    add migration for the teams table

---
+++ database/migrations/2025_05_01_231906_create_teams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('points');
+            $table->integer('costume_rating');
+            $table->smallInteger('number_of_players');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};

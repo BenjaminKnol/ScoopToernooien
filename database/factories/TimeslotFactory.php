c58f160966f7e42cc68d1c155883706aeecc951d
Benjamin Knol <benjamin_knol@knolbeheer.nl>
2025-05-09 18:50:06 +0200

    add timeslots which hold games. currently it holds a single game. should hold more

---
+++ database/factories/TimeslotFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Team;
+use App\Models\Timeslot;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class TimeslotFactory extends Factory
+{
+    protected $model = Timeslot::class;
+
+    public function definition(): array
+    {
+        $nFields = $this->faker->numberBetween(1, 10);
+        $teams = Team::getTeamsByPoules();
+        $poule = $this->faker->randomElement(array_keys($teams));
+        $team1 = $this->faker->randomElement($teams[$poule]);
+        $team2 = $this->faker->randomElement($teams[$poule]);
+        // TODO: find a more clever solution. (pop from array?)
+        while ($team1 == $team2) {
+            $team2 = $this->faker->randomElement($teams[$poule]);
+        }
+        $games = [
+            'team_1' => $team1,
+            'team_2' => $team2,
+        ];
+        return [
+            'fields' => $nFields,
+            'games' => $games,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}

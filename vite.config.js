09c0111d5032ba6b1800b9dae0b2b48e28781a79
Benjamin Knol <Benjamin_knol@hotmail.com>
2025-08-27 21:33:28 +0200

    fix vite build process

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,6 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                // Include images that are referenced directly via Vite::asset in Blade
-                'resources/images/*',
             ],
             refresh: true,
         }),

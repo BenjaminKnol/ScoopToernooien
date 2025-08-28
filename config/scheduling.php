e2f85476380b0d0617e56fc3f95c343b7fa87ebf
Benjamin Knol <Benjamin_knol@hotmail.com>
2025-08-28 03:41:32 +0200

    update env and config

---
+++ config/scheduling.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'number_of_fields' => env('NUMBER_OF_FIELDS'),
+    'available_hours' => env('AVAILABLE_HOURS'),
+    'tournament_day' => env('TOURNAMENT_DAY'),
+    'start_time' => env('START_TIME')
+];
